9ad7b94da1213cdedaffd92e428555bdbd2688b7
SaifAlisoomro <hk4948171@gmail.com>
2024-04-18 15:35:55 +0500

    cli words counter

---
+++ words.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const UserAns : {
+    sentence:string
+}= await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message:"Enter Your Sentence To Count The Word:"
+    }
+])
+const words = UserAns.sentence.trim().split(" ")
+console.log(words);
+console.log(`Your Sentence Word Count Is: ${words.length}`);
